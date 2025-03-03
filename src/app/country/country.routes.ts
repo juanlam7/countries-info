23dd7a3c179db62e736ac272c93479051aa42400
juanlam7 <juanlam7@gmail.com>
2025-03-03 20:06:31 +0100

    feat: Add by country and by region pages with his logic

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
 
 export const countryRoutes: Routes = [
   {
@@ -11,7 +13,14 @@ export const countryRoutes: Routes = [
         path: 'by-capital',
         component: ByCapitalPageComponent,
       },
-
+      {
+        path: 'by-country',
+        component: ByCountryPageComponent,
+      },
+      {
+        path: 'by-region',
+        component: ByRegionPageComponent,
+      },
       {
         path: '**',
         redirectTo: 'by-capital',
---
+++ src/app/country/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, linkedSignal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of } from 'rxjs';
+import { CountryListComponent } from '../../components/country-list/country-list.component';
+import { SearchInputComponent } from '../../components/search-input/search-input.component';
+import { CountryService } from '../../services/country.service';
+
+@Component({
+  selector: 'app-by-country-page',
+  imports: [SearchInputComponent, CountryListComponent],
+  templateUrl: './by-country-page.component.html',
+})
+export class ByCountryPageComponent {
+  countryService = inject(CountryService);
+
+  activatedRoute = inject(ActivatedRoute);
+  router = inject(Router);
+
+  queryParam = this.activatedRoute.snapshot.queryParamMap.get('query') ?? '';
+
+  query = linkedSignal(() => this.queryParam);
+
+  countryResource = rxResource({
+    request: () => ({ query: this.query() }),
+    loader: ({ request }) => {
+      if (!request.query) return of([]);
+
+      this.router.navigate(['/country/by-country'], {
+        queryParams: {
+          query: request.query,
+        },
+      });
+
+      return this.countryService.searchByCountry(request.query);
+    },
+  });
+}

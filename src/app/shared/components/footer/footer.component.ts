291aca204c2711926082499ae8f03f392955c04f
juanlam7 <juanlam7@gmail.com>
2025-03-18 22:47:09 +0100

    feat: footer refactor and text changes

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  capitals = ['Buenos Aires', 'Paris', 'Berlin', 'Amsterdam'];
+  countries = ['Venezuela', 'España', 'Colombia', 'Reino Unido'];
+  regions = ['America', 'Europa', 'Asia', 'Africa'];
+}
